b412a9e352b31fb898eefb490fce8897ec6b1946
moryammony45 <moryammony46@gmail.com>
2025-10-29 20:33:29 +0600

    Delete Practise-4 directory

--- Practise-4/Q1.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  List<String> names = ['Mim', 'Mony', 'Liya', 'Chowa', 'Niha'];
-
-  print('All names:');
-  for (var name in names) {
-    print(name);
-  }
-}
--- Practise-4/Q2.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  Set<String> fruits = {'Avocado', 'Orange', 'Lichi'};
-
-  print('Fruits:');
-  for (var fruit in fruits) {
-    print(fruit);
-  }
-}
